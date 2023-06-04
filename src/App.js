e71ce5d3201f7a3502b880dddb555c3ac18d9c83
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-06-04 21:34:22 +0100

    state done in app and other bits done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+// state
+import React, {useState} from 'react';
+// material ui lib
+// everything needs to be wrapped around ThemeProvider
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/system';
+
+// components
+import AutoCompleteSearchBox from "./components/AutoCompleteSearchBox";
+
 
 function App() {
+// setting up state
+const [movieTitleResults, setMovieTitleResults] = useState([]);
+
+// need to create a theme so the ThemeProvider breaks (I think ThemeProvider is like a container)
+const theme = createTheme();
+
+
   return (
     <>
+    <h1>hello jupiter</h1>
+  
+    <ThemeProvider theme={theme}>
     <div className="App">
-    {/* <h1>hello mars</h1> */}
-
-    
-
-
+    <AutoCompleteSearchBox movieTitleResults={movieTitleResults} setMovieTitleResults={setMovieTitleResults}/>
+      
     </div>
+    </ThemeProvider>
     </>
   );
 }
