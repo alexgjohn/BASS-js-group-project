803c10093e451ce71bcdd507b1122670389337d0
Alex <alexgjohn@outlook.com>
2023-06-05 11:13:23 +0100

    added poster and moved state

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,83 @@ import ErrorPage from './components/ErrorPage';
 import InputPlayerNameBox from './components/InputPlayerNameBox';
 import Footer from './components/Footer';
 
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header';
+import MoviePoster from './components/MoviePoster';
+import MovieForm from './components/MovieForm';
+import LeaderBoard from './components/LeaderBoard';
+import { getUsers, postUser, putUser } from './services/UsersService';
+import UserForm from './components/UserForm';
+import CongratulationsPopupModal from './components/CongratulationsPopUpModal';
+import GameOverPopupModal from './components/GameOverPopUpModal';
+
+const apiKey = "3ca5b3528412adc793325fb27cf4b072";
+const baseUrl = "https://api.themoviedb.org/3";
+const discoverEndpoint = `${baseUrl}/discover/movie?api_key=${apiKey}&with_original_language=en&without_genres=99`;
+
 function App() {
+
+    //generate random number between 1 and 500, use this number to grab a page in this range
+    const randomPage = Math.floor(Math.random() * 500) + 1
+    //generate a random number between 0 and 19, use this as index for above page's list of movies.
+    const randomIndex = Math.floor(Math.random() * 20)
+
+    const [movies, setMovies] = useState([])
+    const [currentPage, setCurrentPage] = useState(randomPage)
+    const [targetMovie, setTargetMovie] = useState(null)
+    const [users, setUsers] = useState([])
+
+    //use effect runs on mount and whenever the current page is changed with setCurrentPage
+    useEffect(() => {
+        getMovies(currentPage)
+    }, [currentPage])
+
+    //this use effect runs on mount and whenever the movie list is updated with setMovies
+    useEffect(() => {
+        console.log("All movie titles:", movies.map(movie => movie.original_title))
+        assignTargetMovie()
+    }, [movies])
+
+    useEffect(() => {
+        getUsers()
+        .then(allUsers => setUsers(allUsers))
+    }, [])
+
+    if (users.length) {
+        console.log("All users:", users)
+    }
+
+
+    const getMovies = function(page) {
+        const pageUrl = `${discoverEndpoint}&page=${page}`
+        fetch(pageUrl)
+        .then(res => res.json())
+        .then(movies => setMovies(movies.results))
+    }
+
+    const assignTargetMovie = () => {
+        setTargetMovie(movies[randomIndex])
+    }
+
+
+    const handleNextPage = () => {
+        setCurrentPage(randomPage)
+    }
+
+    const createUser = newUser => {
+        postUser(newUser)
+        .then(savedUser => setUsers([...users, savedUser]))
+    }
+
+    const updateUserStats = updatedStats => {
+        putUser(updatedStats)
+
+        const updatedUserIndex = users.findIndex(user => user._id === updatedStats._id)
+        const updatedUsers = [...users]
+        updatedUsers[updatedUserIndex] = updatedStats
+        setUsers(updatedUsers)
+    }
+
   return (
     <div className="App">
     <Router>
@@ -21,7 +97,7 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route path="/play" element={<InputPlayerNameBox />} />
         <Route path="/game-rules" element={<GameRules />} />
-        <Route path="game" element={<GameContainer />} />
+        <Route path="game" element={<GameContainer movies={movies} targetMovie={targetMovie} users={users} updateUserStats={updateUserStats}/>} />
         <Route path="/leaderboard" element={<LeaderBoardContainer />} />
         <Route path="*" element={<ErrorPage />} />
       
--- client/src/components/MoviePoster.js
+++ client/src/components/MoviePoster.js
@@ -1,11 +1,27 @@
 
-const MoviePoster= () => {
-    return (
-        <>
-        <h2>This is the MoviePoster</h2>
+import styled from "styled-components"
 
+
+const StyledImage = styled.img`
+    width: 500px;
+    height: 800px;
+`
+
+const MoviePoster = ({targetMovie}) => {
+
+    if (targetMovie === undefined){
+        return null
+    } else if (targetMovie === null) {
+        return null
+    }
+
+    const posterPath = `https://image.tmdb.org/t/p/original${targetMovie.poster_path}`
+
+    return(
+        <>
+            <StyledImage src={posterPath}/>
         </>
-    );
+    )
 }
 
-export default MoviePoster;
+export default MoviePoster
