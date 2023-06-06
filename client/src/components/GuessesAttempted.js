ad1980583ad02e336c23c7c248e642895979c896
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-06-06 02:28:15 +0100

    guesses attempted shown but more logic needed

--- client/src/components/GuessesAttempted.js
+++ client/src/components/GuessesAttempted.js
@@ -1,10 +1,38 @@
-const GuessesAttempted = () => {
-    return (
-        <>
-        <p>This is GuessesAttempted</p>
+import React from 'react';
+import styled from 'styled-components';
 
-        </>
+const StyledGuessesContainer = styled.div`
+    margin-top: 10px;
+    text-decoration: none;
+    color: black; // can change back to green
+`
+// idea: I could use the div and give class names and then target it, will try this tomozzz
+
+
+
+const StyledGuessListItem = styled.li`
+color: ${({ isCorrect }) => (isCorrect ? 'green' : 'red')};
+list-style-type: none;
+
+`
+
+const GuessesAttempted = ({ guesses, targetMovie }) => {
+    return (
+        <StyledGuessesContainer>
+            <div>
+                <h2>Guesses you've made so far:</h2>
+                <ul>
+                    {guesses.map((guess, index) => (
+                        <StyledGuessListItem
+                            key={index} isCorrect={guess === targetMovie}>{guess}
+                        </StyledGuessListItem>
+                    ))}
+                </ul>
+            </div>
+        </StyledGuessesContainer>
     );
-}
+};
 
 export default GuessesAttempted;
+
+
